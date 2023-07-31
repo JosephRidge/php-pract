04d4cd5895d7ba6e3dce90e686e45066426b3f35
josephridge <josephridge18@gmail.com>
2023-07-31 11:34:10 +0300

    Update varibls

--- variables.php
+++ variables.php
@@ -17,10 +17,47 @@ Rules:
 - must start with letter or underscore
 - case sensitive
 - each statement must end with a semicolon*/
+$firstName = "Ericsson";
+$lastName = "Lab"; //string 
+$age = 10; //integer
+$weight = 44.4; // float
+$isOpen = true; // boolean
+$object =[
+    'name'=>'Ericcson',
+    ''
+];
 
-$name = "Huge";
-echo($name);
-var_dump($name); // check out type
+//$aboutMe ="$firstName-$lastName  @$age years old.";
+$aboutMe =$firstName."-".$lastName;
+//echo($aboutMe."<br>");
+//echo("<h1> Hello ".$firstName.", </h1>");
+//var_dump($firstName);
+//var_dump($isOpen);
+
+
+/*
+is_bool()
+is_integer()
+is_string()
+is_null()
+*/
+/*
+if(is_bool(true)){
+    echo ("true");
+}
+else{
+    echo("gfalse");
+}
+echo(is_bool(10));
+
+*/
+
+
+
+
+// $name = "Huge";
+// echo($name);
+// var_dump($name); // check out type
 // concatenation inline and use of (.)
 // mathematical operations
 // +, / ,%,*
